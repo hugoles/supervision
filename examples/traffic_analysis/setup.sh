121e0c311bc4ae8f9ee2d407905f42e113b39796
Hugo Dutra <67278688+hugoles@users.noreply.github.com>
2023-11-17 14:46:59 -0300

    Update setup.sh

--- examples/traffic_analysis/setup.sh
+++ examples/traffic_analysis/setup.sh
@@ -10,8 +10,6 @@ else
     echo "'data' directory already exists."
 fi
 
-# Download the traffic_analysis.mov file from Google Drive
-gdown -O "$DIR/data/traffic_analysis.mov" "https://drive.google.com/uc?id=1qadBd7lgpediafCpL_yedGjQPk-FLK-W"
 
-# Download the traffic_analysis.pt file from Google Drive
-gdown -O "$DIR/data/traffic_analysis.pt" "https://drive.google.com/uc?id=1y-IfToCjRXa3ZdC1JpnKRopC7mcQW-5z"
+# Download the model4.pt file from Google Drive
+gdown -O "$DIR/data/traffic_analysis.pt" "https://drive.google.com/uc?id=1EZ7G1nTG6ezWZZGteuvmnOlCuLz7i0R2"
